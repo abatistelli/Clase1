80bb181ab611413c753c9680d161cd5a4a190e37
Agustin <agustin.batistelli@mercadolibre.com>
2021-12-16 12:04:30 -0300

    subiendo contenido de la clase1

---
+++ ejercicio1/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	nombre := "Agustin"
+	direccion := "calle falsa 123"
+
+	fmt.Println("Hola! soy " + nombre + " y vivo en " + direccion)
+}
---
+++ ejercicio3/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	var nombre string // error porque arranca con un numero
+	var apellido string
+	var edad int          // primero va el nombre luego el tipo
+	apellido = "apellido" // tambien comienza con un numero y aparte ya la declaro arriba como string
+	var licenciaDeConducir = true
+	var estaturaDeLaPersona int // error porque esta mal definidas la variable
+	cantidadDeHijos := 2
+
+}
