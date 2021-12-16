cf5042a4b93d0cc5e879c899d78ad1028452aa36
Agustin <agustin.batistelli@mercadolibre.com>
2021-12-16 17:40:21 -0300

    modificiaciones Clase 1 TM

--- ejercicio4/main.go
+++ ejercicio4/main.go
@@ -1,9 +1,9 @@
 package main
 
 func main() {
-	var apellido string = "Gomez"
-	var edad uint8 = 35
-	boolean := false
-	var sueldo float64 = 45857.90
-	var nombre string = "Julián"
+	//var apellido string = "Gomez"
+	//var edad uint8 = 35
+	//boolean := false
+	//var sueldo float64 = 45857.90
+	//var nombre string = "Julián"
 }
